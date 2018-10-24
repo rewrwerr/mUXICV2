335627386e8d7fecf684c9cd54669558d381d303
rewrwerr <42406979+rewrwerr@users.noreply.github.com>
2018-10-24 23:18:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,47 @@
-client.on('voiceStateUpdate', (u, member) => {
-  var parent = '479601885029072896';
-  var channel = '479602069620260894';
-  if(member.voiceChannel === null || member.voiceChannel !== member.guild.channels.get(channel)) return console.log(`${member.user.username}'s channel isnt the needed one.`);
-  member.guild.createChannel(`${member.user.username}`, 'voice').then(c => {
-    if(!c) return;
-    c.setParent(parent);
-    member.setVoiceChannel(c);
-    setInterval(() => {
-      if(!c) return;
-      if(c.members.size === 0) {
-        c.delete();
-      }
-    }, 30000);
-  });
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = "-";
+client.on('ready', () => {
+console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`PRADA BC`,"http://twitch.tv/S-F")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
 });
 
 
 
+client.on("message", message => {
 
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
 
 
 
